2f1ac9bf9c5051983b176363c6ed5fddd11c7bf7
github username <scott.gloyna01@gmail.com>
2021-01-27 14:12:06 -0600

    very minor changes

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,12 +1,14 @@
 class Employee < ActiveRecord::Base
 
     belongs_to :employer
-    belongs_to :project
     has_many :employeeskills
-    has_many :employeeprojects
     has_many :skills, through: :employeeskills
+    has_many :employeeprojects
     has_many :projects, through: :employeeprojects
+    #belongs_to :project
     #has_many :skills
+
+
    
 
 end
