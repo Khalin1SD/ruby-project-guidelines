c41681b8a2f325fe6cb40a1b42a0e225e337d758
github username <scott.gloyna01@gmail.com>
2021-01-27 19:33:50 -0600

    what_can_i_do?

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -8,6 +8,33 @@ class Employee < ActiveRecord::Base
     #belongs_to :project
     #has_many :skills
 
+    def self.who_can_do_this?(this_skill)
+        #takes in the name of a skill (string) and prints the list of employee names
+
+        employee_list = Employee.joins(:skills).where(skills: {name: this_skill})
+        employee_list.each {|employee| puts employee.name}
+        #binding.pry
+    end
+
+
+    def what_can_i_do?
+
+        skills_with_comp = {}
+        competency_list = self.employeeskills.map {|link| link.competency}
+        skills_list = self.employeeskills.map {|link| Skill.find(link.skill_id).name}
+        skills_list.zip(competency_list) {|skills,comp| skills_with_comp[skills] = comp}
+
+        puts self.name + "\n"
+        x=skills_list.length
+        i=0
+        x.times do
+            puts skills_list[i].to_s + "\t" + competency_list[i].to_s
+            i+=1
+        end
+
+        skills_with_comp
+    end
+
 
    
 
