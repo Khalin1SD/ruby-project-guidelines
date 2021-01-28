ffa6bd41e0cf2d825cedfdd6237fc39a1481495a
Khalin Redding <khalinredd@gmail.com>
2021-01-28 10:34:16 -0500

    add_skill method works

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -8,6 +8,22 @@ class Employee < ActiveRecord::Base
     #belongs_to :project
     #has_many :skills
 
+    def add_skill(skill, comp)
+        if Skill.all.map {|this| this.name}.include?("#{skill}") == false
+        puts "Sorry skill does not exist. See below."
+        puts Skill.all.map {|that| that.name}
+        
+        elsif (Skill.all.map {|this| this.name}.include?("#{skill}") == true) && (self.skills.find_by(name: skill))
+            puts "Already has skill"
+            
+            # self.skills.find_by(name: skill)
+            
+        else
+            this_skill = Skill.all.find_by(name: "#{skill}").id
+            Employeeskill.create(employee_id:self.id, skill_id: this_skill, competency: comp)
+        end
+
+    end
 
    
 
